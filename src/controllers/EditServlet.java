97ce63856ea2bf6218a133bb03e1825128bd1975
yoshiki.tanizawa <beautifulsunset777@gmail.com>
2024-02-29 23:08:07 +0900

    存在しないidに対する処理を追加

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -18,8 +18,8 @@ import utils.DBUtil;
  */
 @WebServlet("/edit")
 public class EditServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,26 +27,33 @@ public class EditServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// テキストに則り、コード変更
-		EntityManager em = DBUtil.createEntityManager();
-		
-		// 該当のIDのメッセージ1件のみをデータベースから取得
-		Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
-		
-		em.close();
-		
-		// メッセージ情報とセッションIDをリクエストスコープに登録
-		request.setAttribute("message", m);
-		request.setAttribute("_token", request.getSession().getId());
-		
-		// メッセージIDをセッションスコープに登録
-		request.getSession().setAttribute("message_id", m.getId());
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
-		rd.forward(request, response);	
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // テキストに則り、コード変更
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 該当のIDのメッセージ1件のみをデータベースから取得
+        Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        // メッセージ情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("message", m);
+        request.setAttribute("_token", request.getSession().getId());
+
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("message_id", m.getId());
+
+        // メッセージデータが存在しているときのみ
+        // メッセージIDをセッションスコープに登録
+        if (m != null) {
+            request.getSession().setAttribute("message_id", m.getId());
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+        rd.forward(request, response);
+    }
 }
