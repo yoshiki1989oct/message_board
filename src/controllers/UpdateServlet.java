2f7a97a5368eba98dcb25db4e71f00c73a64c9b2
yoshiki.tanizawa <beautifulsunset777@gmail.com>
2024-03-03 21:00:44 +0900

    バリデーションを追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
+import models.validators.MessageValidator;
 import utils.DBUtil;
-
 
 /**
  * Servlet implementation class UpdateServlet
@@ -50,22 +52,31 @@ public class UpdateServlet extends HttpServlet {
 
         Timestamp currentTime = new Timestamp(System.currentTimeMillis());
         m.setUpdated_at(currentTime);
-        // 上記は、更新日時のみ上書き？
+        // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
+        List<String> errors = MessageValidator.validate(m);
+        if(errors.size() > 0) {
+            em.close();
 
-        //データベースを更新
-        em.getTransaction().begin();
-        em.getTransaction().commit();
+            // フォームに初期値を設定、さらにエラーメッセージを送る
+            request.setAttribute("_token", request.getSession().getId());
+            request.setAttribute("message", m);
+            request.setAttribute("errors", errors);
 
-        // フラッシュメッセージをセッションスコープから取り出し、表示するコードを追加
-        request.getSession().setAttribute("flush", "更新が完了しました。");
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+            rd.forward(request, response);
+        } else {
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "更新が完了しました。");
+            em.close();
 
-        em.close();
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("message_id");
 
-        // セッションスコープ上の不要になったデータを削除
-        request.getSession().removeAttribute("message_id");
-
-        // indexページへリダイレクト
-        response.sendRedirect(request.getContextPath() + "/index");
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
         }
     }
 }
